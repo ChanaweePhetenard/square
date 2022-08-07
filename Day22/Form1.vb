8ca3a2400a5bc42b5fc457e18726bee1eb6b0a4c
ChanaweePhetenard <109448805+ChanaweePhetenard@users.noreply.github.com>
2022-08-07 14:18:13 +0700

    หาพื้นที่สีเหลี่ยม

---
+++ Day22/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox3.Text = Val(TextBox1.Text) * Val(TextBox2.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+    End Sub
+End Class
